ce560638e994cb1ae7b9eaa7a5c0f431045df63b
nice-gift <dmitry.luschick@yandex.by>
2022-06-29 16:03:10 +0300

    copy ooptask in package lesson23

---
+++ src/by/kursy/luschik/javalessons/lesson23/ooptask/model/entity/Gladiolus.java
@@ -0,0 +1,89 @@
+package by.kursy.luschik.javalessons.lesson23.ooptask.model.entity;
+
+public class Gladiolus extends Flower {
+    private int numFlwrsInInflr;
+    private int numOpenFlwrsInInflr;
+    private String inflrType;
+
+/* Соцветия гладиолусов: 1 — однорядное; 2 — двухрядное; 3 — очередное;
+4 — спиральное; 5 — двустороннее
+Inflorescences of gladioli: 1 - single row; 2 - two-row; 3 - next;
+4 - spiral; 5 - bilateral */
+
+    public Gladiolus() {
+        numFlwrsInInflr = 0;
+        numOpenFlwrsInInflr = 0;
+        inflrType = "undefined";
+    }
+
+    public Gladiolus(String name, String color, double price, double weight,
+                     int length, int numFlwrsInInflr, int numOpenFlwrsInInflr,
+                     String inflrType) {
+        super(name, color, price, weight, length);
+        this.numFlwrsInInflr = numFlwrsInInflr;
+        this.numOpenFlwrsInInflr = numOpenFlwrsInInflr;
+        this.inflrType = inflrType;
+    }
+
+    public Gladiolus(Gladiolus gladiolus) {
+        this(gladiolus.getName(), gladiolus.getColor(), gladiolus.getPrice(),
+                gladiolus.getWeight(), gladiolus.getLength(),
+                gladiolus.numFlwrsInInflr, gladiolus.numOpenFlwrsInInflr,
+                gladiolus.inflrType);
+    }
+
+    public int getNumFlwrsInInflr() {
+        return numFlwrsInInflr;
+    }
+
+    public void setNumFlwrsInInflr(int numFlwrsInInflr) {
+        if (numFlwrsInInflr >= 0 && numFlwrsInInflr < 50) {
+            this.numFlwrsInInflr = numFlwrsInInflr;
+        }
+    }
+
+    public int getNumOpenFlwrsInInflr() {
+        return numOpenFlwrsInInflr;
+    }
+
+    public void setNumOpenFlwrsInInflr(int numOpenFlwrsInInflr) {
+        if (numOpenFlwrsInInflr >= 0 && numOpenFlwrsInInflr <= numFlwrsInInflr) {
+            this.numOpenFlwrsInInflr = numOpenFlwrsInInflr;
+        }
+    }
+
+    public String getInflrType() {
+        return inflrType;
+    }
+
+    public void setInflrType(String inflrType) {
+        this.inflrType = inflrType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!super.equals(o)) return false;
+
+        Gladiolus gladiolus = (Gladiolus) o;
+
+        if (numFlwrsInInflr != gladiolus.numFlwrsInInflr) return false;
+        if (numOpenFlwrsInInflr != gladiolus.numOpenFlwrsInInflr) return false;
+        return inflrType.equals(gladiolus.inflrType);
+    }
+
+    @Override
+    public int hashCode() {
+        int code = 31;
+        int hash = super.hashCode() * code + numFlwrsInInflr;
+        hash = hash * code + numOpenFlwrsInInflr;
+        hash = hash * code + inflrType.hashCode();
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", numFlwrsInInflr = " +
+                numFlwrsInInflr + ", numOpenFlwrsInInflr = " +
+                numOpenFlwrsInInflr + ", inflrType = " + inflrType;
+    }
+}
---
+++ src/by/kursy/luschik/javalessons/lesson23/ooptask/model/entity/Sunflower.java
@@ -0,0 +1,66 @@
+package by.kursy.luschik.javalessons.lesson23.ooptask.model.entity;
+
+public class Sunflower extends Flower {
+    private int headDiameter;
+    private int numOfLeaves;
+
+    public Sunflower() {
+    }
+
+    public Sunflower(String name, String color, double price, double weight,
+                     int length, int headDiameter, int numOfLeaves) {
+        super(name, color, price, weight, length);
+        this.headDiameter = headDiameter;
+        this.numOfLeaves = numOfLeaves;
+    }
+
+    public Sunflower(Sunflower sunflower) {
+        this(sunflower.getName(), sunflower.getColor(), sunflower.getPrice(),
+                sunflower.getWeight(), sunflower.getLength(),
+                sunflower.headDiameter, sunflower.numOfLeaves);
+    }
+
+    public int getHeadDiameter() {
+        return headDiameter;
+    }
+
+    public void setHeadDiameter(int headDiameter) {
+        if (headDiameter >= 0 && headDiameter <= 50) {
+            this.headDiameter = headDiameter;
+        }
+    }
+
+    public int getNumOfLeaves() {
+        return numOfLeaves;
+    }
+
+    public void setNumOfLeaves(int numOfLeaves) {
+        if (numOfLeaves >= 0 && numOfLeaves <= 20) {
+            this.numOfLeaves = numOfLeaves;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!super.equals(o)) return false;
+
+        Sunflower sunflower = (Sunflower) o;
+
+        if (headDiameter != sunflower.headDiameter) return false;
+        return numOfLeaves != sunflower.numOfLeaves;
+    }
+
+    @Override
+    public int hashCode() {
+        int code = 31;
+        int hash = super.hashCode() * code + headDiameter;
+        hash = hash * code + numOfLeaves;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Sunflower " + super.toString() + ", headDiameter = " + headDiameter +
+                ", numOfLeaves = " + numOfLeaves;
+    }
+}
