96ab17e2f5926f45d8347dc5c415ec40d1fb48f8
nice-gift <dmitry.luschick@yandex.by>
2022-10-07 14:03:26 +0300

    new project Port

---
+++ src/by/kursy/luschik/javalessons/stage30/controller/Main.java
@@ -0,0 +1,7 @@
+package by.kursy.luschik.javalessons.stage30.controller;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/by/kursy/luschik/javalessons/stage30/model/entity/Ship.java
@@ -0,0 +1,20 @@
+package by.kursy.luschik.javalessons.stage30.model.entity;
+
+public class Ship {
+    public static final int MAX_SHIP_CONTAINER_CAPACITY = 10;
+
+    private String name;
+    private int numOfContainers;
+
+    public Ship() {
+        numOfContainers = 0;
+        name = "no name";
+    }
+
+    public Ship(String name, int numOfContainers) {
+        this.name = name;
+        this.numOfContainers = numOfContainers;
+    }
+
+
+}
