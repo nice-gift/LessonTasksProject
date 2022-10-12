d49dde7423f0d16547e901d796bb8c9ceb8e1df3
nice-gift <100232175+nice-gift@users.noreply.github.com>
2022-10-12 20:42:31 +0300

    using blocked synchr with Semaphore (acquire(), release())

--- src/by/kursy/luschik/javalessons/lesson33/model/Printer.java
+++ src/by/kursy/luschik/javalessons/lesson33/model/Printer.java
@@ -1,34 +1,39 @@
 package by.kursy.luschik.javalessons.lesson33.model;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class Printer {
-    private Lock lock;
+
+    private Semaphore semaphore;
 
     public Printer() {
-        lock = new ReentrantLock();
+        semaphore = new Semaphore(5, true);
     }
 
-    public Lock getLock() {
-        return lock;
-    }
 
     public void print(String text) {
-
-        System.out.print("[");
-
         try {
-            TimeUnit.MILLISECONDS.sleep(100);
-            for (int i = 0; i < text.length(); i++) {
-                System.out.print(text.charAt(i));
+            semaphore.acquire();
+            System.out.print("[");
+
+            try {
                 TimeUnit.MILLISECONDS.sleep(100);
+
+                for (int i = 0; i < text.length(); i++) {
+                    System.out.print(text.charAt(i));
+                    TimeUnit.MILLISECONDS.sleep(100);
+                }
+
+            } catch (InterruptedException e) {
+                System.err.println(e);
             }
+            System.out.println("]");
         } catch (InterruptedException e) {
             System.err.println(e);
+        } finally {
+            semaphore.release();
         }
-        System.out.println("]");
     }
 }
 
--- src/by/kursy/luschik/javalessons/lesson33/model/Writer.java
+++ src/by/kursy/luschik/javalessons/lesson33/model/Writer.java
@@ -20,24 +20,6 @@ public class Writer implements Runnable {
 
     @Override
     public void run() {
-        int count = 0;
-        while (true) {
-            if (printer.getLock().tryLock()) {
-                try {
-                    printer.print(text);
-                    System.out.printf("\n %s-thread: count = %d\n", text, count);
-                    break;
-                } finally {
-                    printer.getLock().unlock();
-                }
-            } else {
-                count++;
-                try {
-                    TimeUnit.MILLISECONDS.sleep(100);
-                } catch (InterruptedException e) {
-                    System.err.println(e);
-                }
-            }
-        }
+        printer.print(text);
     }
 }
